afddb9ebfb47387374507f8b731caec14425aaf1
Sara Tarnvik <stvik2@gmail.com>
2019-10-16 13:47:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,11 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  array.each_with_index{|name,i| puts "#{i+1} #{name}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
+  new_ arr = array.map{|word| "#{word.capitalize}!"}
+  new_arr
   # Your code here
 end
 
