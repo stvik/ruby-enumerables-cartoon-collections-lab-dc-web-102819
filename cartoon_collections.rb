81ab0594f6765949e1533e8736ea66c38575ef82
Sara Tarnvik <stvik2@gmail.com>
2019-10-16 14:03:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,9 @@ def summon_captain_planet(array)# code an argument here
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  
 end
 
 def find_the_cheese# code an argument here
